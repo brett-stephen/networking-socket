a76860543766939609d4f70817b18e8b3a309c12
Darren (Darren) Bartel <bard3780@36starlord.cs.uleth.ca>
2018-11-13 14:36:52 -0700

    stud

--- client.cpp
+++ client.cpp
@@ -9,16 +9,26 @@ int main(int argc, int argv[])
       // Replace "localhost" with the hostname
       // that you're running your server.
       ClientSocket client_socket("localhost", 30000);
-      std::string reply;
-      // Usually in real applications, the following
-      // will be put into a loop. 
-      try {
-	 client_socket << "Test message.";
-	 client_socket >> reply;
+      ClientSocket client_socket2("localhost", 30001);
+      
+      while(true){ 
+	 std::string reply;
+	 // Usually in real applications, the following
+	 // will be put into a loop. 
+	 try {
+	    client_socket << "Test message.";
+	    client_socket >> reply;
+	    client_socket2 << "ACK";
+	 }
+	 catch(SocketException&){
+	 }
+	 std::cout << "We received this response from the server:\n\"" << reply << "\"\n";
+      
       }
-      catch(SocketException&){
-      }
-      std::cout << "We received this response from the server:\n\"" << reply << "\"\n";;
+
+
+      
+      
    }
    catch(SocketException& e){
       std::cout << "Exception was caught:" << e.description() << "\n";
--- server.cpp
+++ server.cpp
@@ -1,17 +1,22 @@
 #include "ServerSocket.h"
 #include "SocketException.h"
 #include <string>
+#include<iostream>
  
 int main(int argc, int argv[])
 {
-   std::cout << "running....\n";
+   std::cout << "runningData....\n";
    try{
       // Create the socket
       ServerSocket server(30000);
+      ServerSocket Ack(30001);
 
       while (true){
 	 ServerSocket new_sock;
 	 server.accept(new_sock);
+
+	 ServerSocket new_sock2;
+	 Ack.accept(new_sock2);
 
 	 // For multiple threading, you need to create
 	 // a new thread here and pass new_sock to it.
@@ -19,9 +24,18 @@ int main(int argc, int argv[])
 	 // with the client.
 	 try{
 	    while (true){
-	       std::string data;
-	       new_sock >> data;
+	       std::string data = "Lethbridge";
+	       std::string ack;
 	       new_sock << data;
+	       new_sock2 >> ack;
+	       if (ack == "ACK") {
+		  std::cout << "Good" << std::endl;
+		   std::cout<<ack<<std::endl;
+	       } else{
+		  std::cout << "Bad" << std::endl;
+		  std::cout<<ack<<std::endl;
+	       }
+	       
 	    }
 	 }
 	 catch(SocketException&){
@@ -31,5 +45,7 @@ int main(int argc, int argv[])
    catch (SocketException& e){
       std::cout << "Exception was caught:" << e.description() << "\nExiting.\n";
    }
+
+      
   return 0;
 }
